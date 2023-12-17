06d95c106b93a946876b7cec249d010de057bc2e
CieslewiczEryk <CieslewiczEryk@users.noreply.github.com>
2023-12-17 13:01:08 +0100

    Changed name and msg

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Natalia", "msg":"Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Eryk", "msg":"wants to wish u a Merry Christmas and Happy New Year!"}', rv.data)
